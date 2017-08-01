6026f4ed97da7a29fcead6328cf0206868099c24
马军伟 <majunwei@weidian.com>
2017-08-01 09:27:06 +0800

    first

---
+++ srping-boot-tutorial-first/src/main/java/org/srping/boot/tutorial/first/Application.java
@@ -0,0 +1,26 @@
+package org.srping.boot.tutorial.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello word实例
+ * @author majunwei
+ *
+ */
+@RestController
+@EnableAutoConfiguration
+public class Application {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
