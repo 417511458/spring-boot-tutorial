8511593e26189d1447f1a3711a4ee9da59f7f19a
马军伟 <majunwei@weidian.com>
2017-08-07 18:06:06 +0800

    non-executable

---
+++ spring-boot-tutorial-non-executable/src/main/java/org/spring/boot/tutorial/non/executable/Application.java
@@ -0,0 +1,13 @@
+package org.spring.boot.tutorial.non.executable;
+
+/**
+ * Non-Executable 实例
+ * @author majunwei
+ *
+ */
+public class Application {
+	public void testNonExecutable(){
+		System.out.println("OK");
+	}
+	
+}
